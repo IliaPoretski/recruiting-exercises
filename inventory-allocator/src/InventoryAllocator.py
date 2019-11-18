4033f49f4e7ad2ed28b5cf49150d5b65a6b59cfb
Ilia <iliaporetski@gmail.com>
2019-11-18 18:13:14 -0500

    Cleaned up code, and added comments

--- inventory-allocator/src/InventoryAllocator.py
+++ inventory-allocator/src/InventoryAllocator.py
@@ -1,39 +1,54 @@
-from collections import OrderedDict
+from copy import deepcopy
+
 
 class InventoryAllocator:
+    """
+    Class used to allocate inventory based on order, and warehouse inventory.
 
-	def __init__(self, orders, warehouses):
-		self.orders = orders
-		self.warehouses= warehouses
-		
-	def allocateInventory(self):
-		shipments = []
-		for order, amount in self.orders.items():
-			for warehouse in self.warehouses:
-				if amount == 0:
-					break
-				if order in warehouse['inventory']:
-					if amount <= warehouse['inventory'].get(order):
-						for shipment in shipments:
-							if shipment.get(warehouse['name']):
-								shipment[warehouse['name']].update({order: amount})
-								break
-						else:
-							shipments.append({warehouse['name']: {order: amount}})
-						amount -= warehouse['inventory'].get(order)
-					else: 
-						available = warehouse['inventory'].get(order)
-						for shipment in shipments:
-							if shipment.get(warehouse['name']):
-								shipment[warehouse['name']].update({order: available})
-								break
-						else:
-							shipments.append({warehouse['name']: {order: available}})
-						amount -= available
-			if amount > 0:
-				return []
-		return shipments
+    Attributes
+    ----------
+    warehouses: list
+        A list of warehouses. each warehouse has a name and inventory.
 
-		
+    Methods
+    -------
+    allocateInventory(order_input: dict) -> list
+        Returns the allocated inventory
+        based on all warehouse inventory and order_input.
 
+    """
+    def __init__(self, warehouses: list):
+        self.warehouses = warehouses
 
+    def allocateInventory(self, order_input: dict) -> list:
+        # Create copy of order_input to not make changes to argument.
+        order = deepcopy(order_input)
+        allocations = []
+        for warehouse in self.warehouses:
+            # Stops iterating warehouses if all inventory allocated
+            if all(units <= 0 for units in order.values()):
+                break
+            for item, units, in order.items():
+                # If item is in warehouse with enough inventory and unallocated
+                if (item in warehouse['inventory'] and
+                        warehouse['inventory'].get(item) > 0 and
+                        units > 0):
+                    units_allocated = min(units,
+                                          warehouse['inventory'].get(item))
+                    # Add to allocation if warehouse exists in allocations
+                    for allocation in allocations:
+                        if allocation.get(warehouse['name']):
+                            allocation[warehouse['name']].\
+                                update({item: units_allocated})
+                            break
+                    # Else add warehouse to allocation
+                    else:
+                        allocations.append({warehouse['name']:
+                                           {item: units_allocated}})
+                    # Update amount of units still required in order
+                    order[item] = units - units_allocated
+        # If order is not fulfilled return empty list
+        if not all(units <= 0 for units in order.values()):
+            return []
+        else:
+            return allocations
--- inventory-allocator/src/InventoryAllocatorTest.py
+++ inventory-allocator/src/InventoryAllocatorTest.py
@@ -1,139 +1,149 @@
 import unittest
 from InventoryAllocator import InventoryAllocator
 
+
 class TestInventoryAllocator(unittest.TestCase):
 
-	def test_basic(self):
-		orders = {'apple': 1}
-		warehouses = [{'name': 'wh1', 'inventory': {'apple': 1}}]
-		expected = [{'wh1': {'apple': 1 }}]
-		inventoryAllocator = InventoryAllocator(orders, warehouses)
-		self.assertEqual(inventoryAllocator.allocateInventory(), expected)
+    def test_basic(self):
+        order = {'apple': 1}
+        warehouses = [{'name': 'wh1', 'inventory': {'apple': 1}}]
+        expected = [{'wh1': {'apple': 1}}]
+        inventoryAllocator = InventoryAllocator(warehouses)
+        self.assertEqual(inventoryAllocator.allocateInventory(order), expected)
 
-	def test_excess_inventory(self):
-		orders = {'apple': 1}
-		warehouses = [{'name': 'wh1', 'inventory': { 'apple': 2 } }]
-		expected = [{'wh1': {'apple': 1}}]
-		inventoryAllocator = InventoryAllocator(orders, warehouses)
-		self.assertEqual(inventoryAllocator.allocateInventory(), expected)
+    def test_excess_inventory(self):
+        order = {'apple': 1}
+        warehouses = [{'name': 'wh1', 'inventory': {'apple': 2}}]
+        expected = [{'wh1': {'apple': 1}}]
+        inventoryAllocator = InventoryAllocator(warehouses)
+        self.assertEqual(inventoryAllocator.allocateInventory(order), expected)
 
-	def test_empty_order_no_warehouses(self):
-		orders = {}
-		warehouses = []
-		expected = []
-		inventoryAllocator = InventoryAllocator(orders, warehouses)
-		self.assertEqual(inventoryAllocator.allocateInventory(), expected)	
+    def test_empty_order_no_warehouses(self):
+        order = {}
+        warehouses = []
+        expected = []
+        inventoryAllocator = InventoryAllocator(warehouses)
+        self.assertEqual(inventoryAllocator.allocateInventory(order), expected)
 
-	def test_no_warehouses(self):
-		orders = {'apple': 10}
-		warehouses = []
-		expected = []
-		inventoryAllocator = InventoryAllocator(orders, warehouses)
-		self.assertEqual(inventoryAllocator.allocateInventory(), expected)	
+    def test_no_warehouses(self):
+        order = {'apple': 10}
+        warehouses = []
+        expected = []
+        inventoryAllocator = InventoryAllocator(warehouses)
+        self.assertEqual(inventoryAllocator.allocateInventory(order), expected)
 
-	def test_empty_order(self):
-		orders = {}
-		warehouses = [{'name': 'wh1', 'inventory': {'apple': 5 }}]
-		expected = []
-		inventoryAllocator = InventoryAllocator(orders, warehouses)
-		self.assertEqual(inventoryAllocator.allocateInventory(), expected)	
+    def test_empty_order(self):
+        order = {}
+        warehouses = [{'name': 'wh1', 'inventory': {'apple': 5}}]
+        expected = []
+        inventoryAllocator = InventoryAllocator(warehouses)
+        self.assertEqual(inventoryAllocator.allocateInventory(order), expected)
 
-	def test_zero_order(self):
-		orders = {'apple': 0}
-		warehouses = [{'name': 'wh1', 'inventory': {'apple': 1}}]
-		expected = []
-		inventoryAllocator = InventoryAllocator(orders, warehouses)
-		self.assertEqual(inventoryAllocator.allocateInventory(), expected)
+    def test_zero_order(self):
+        order = {'apple': 0}
+        warehouses = [{'name': 'wh1', 'inventory': {'apple': 1}}]
+        expected = []
+        inventoryAllocator = InventoryAllocator(warehouses)
+        self.assertEqual(inventoryAllocator.allocateInventory(order), expected)
 
-	def test_no_inventory(self):
-		orders = {'apple': 1}
-		warehouses = [{'name': 'wh1', 'inventory': {'apple': 0}}]
-		expected = []
-		inventoryAllocator = InventoryAllocator(orders, warehouses)
-		self.assertEqual(inventoryAllocator.allocateInventory(), expected)
+    def test_no_inventory(self):
+        order = {'apple': 1}
+        warehouses = [{'name': 'wh1', 'inventory': {'apple': 0}}]
+        expected = []
+        inventoryAllocator = InventoryAllocator(warehouses)
+        self.assertEqual(inventoryAllocator.allocateInventory(order), expected)
 
-	def test_zero_order_no_inventory(self):
-		orders = {'apple': 0}
-		warehouses = [{'name': 'wh1', 'inventory': {'apple': 0}}]
-		expected = []
-		inventoryAllocator = InventoryAllocator(orders, warehouses)
-		self.assertEqual(inventoryAllocator.allocateInventory(), expected)
+    def test_zero_order_no_inventory(self):
+        order = {'apple': 0}
+        warehouses = [{'name': 'wh1', 'inventory': {'apple': 0}}]
+        expected = []
+        inventoryAllocator = InventoryAllocator(warehouses)
+        self.assertEqual(inventoryAllocator.allocateInventory(order), expected)
 
-	def test_no_item_in_warehouse(self):
-		orders = {'apple': 1}
-		warehouses = [{'name': 'wh1', 'inventory': {'orange': 10}}]
-		expected = []
-		inventoryAllocator = InventoryAllocator(orders, warehouses)
-		self.assertEqual(inventoryAllocator.allocateInventory(), expected)
+    def test_no_item_in_warehouse(self):
+        order = {'apple': 1}
+        warehouses = [{'name': 'wh1', 'inventory': {'orange': 10}}]
+        expected = []
+        inventoryAllocator = InventoryAllocator(warehouses)
+        self.assertEqual(inventoryAllocator.allocateInventory(order), expected)
 
-	def test_multiple_warehouses_split(self):
-		orders = { 'apple': 10 }
-		warehouses = [{'name': 'wh1', 'inventory': {'apple': 5}},
-					  {'name': 'wh2', 'inventory': {'apple': 5}}]
-		expected = [{'wh1': {'apple': 5 }}, {'wh2': {'apple': 5}}]
-		inventoryAllocator = InventoryAllocator(orders, warehouses)
-		self.assertEqual(inventoryAllocator.allocateInventory(), expected)
+    def test_multiple_warehouses_split(self):
+        order = {'apple': 10}
+        warehouses = [{'name': 'wh1', 'inventory': {'apple': 5}},
+                      {'name': 'wh2', 'inventory': {'apple': 5}}]
+        expected = [{'wh1': {'apple': 5}}, {'wh2': {'apple': 5}}]
+        inventoryAllocator = InventoryAllocator(warehouses)
+        self.assertEqual(inventoryAllocator.allocateInventory(order), expected)
 
-	def test_skip_empty_warehouses(self):
-		orders = {'apple': 10}
-		warehouses = [{'name': 'wh1', 'inventory': {'apple': 0}}, 
-					  {'name': 'wh2', 'inventory': {'apple': 10}}]
-		expected = [{'wh2': {'apple': 10}}]
-		inventoryAllocator = InventoryAllocator(orders, warehouses)
-		self.assertEqual(inventoryAllocator.allocateInventory(), expected)	
+    def test_skip_empty_warehouses(self):
+        order = {'apple': 10}
+        warehouses = [{'name': 'wh1', 'inventory': {'apple': 0}},
+                      {'name': 'wh2', 'inventory': {'apple': 10}}]
+        expected = [{'wh2': {'apple': 10}}]
+        inventoryAllocator = InventoryAllocator(warehouses)
+        self.assertEqual(inventoryAllocator.allocateInventory(order), expected)
 
-	def test_multiple_warehouses_by_cost(self):
-		orders = {'apple': 10}
-		warehouses = [{'name': 'wh1', 'inventory': {'apple': 3}}, 
-					  {'name': 'wh2', 'inventory': {'apple': 1}},
-					  {'name': 'wh3', 'inventory': {'apple': 7}}]
-		expected = [{'wh1': {'apple': 3}},
-					{'wh2': {'apple': 1}}, 
-					{'wh3': {'apple': 6}}]
-		inventoryAllocator = InventoryAllocator(orders, warehouses)
-		self.assertEqual(inventoryAllocator.allocateInventory(), expected)
+    def test_multiple_warehouses_by_cost(self):
+        order = {'apple': 10}
+        warehouses = [{'name': 'wh1', 'inventory': {'apple': 3}},
+                      {'name': 'wh2', 'inventory': {'apple': 1}},
+                      {'name': 'wh3', 'inventory': {'apple': 7}}]
+        expected = [{'wh1': {'apple': 3}},
+                    {'wh2': {'apple': 1}},
+                    {'wh3': {'apple': 6}}]
+        inventoryAllocator = InventoryAllocator(warehouses)
+        self.assertEqual(inventoryAllocator.allocateInventory(order), expected)
 
-	def test_multiple_order(self):
-		orders = {'apple': 10 , 'orange': 5}
-		warehouses = [{'name': 'wh1', 'inventory': {'apple': 10, 'orange': 5}}]
-		expected = [{'wh1': {'apple': 10, 'orange': 5}}]
-		inventoryAllocator = InventoryAllocator(orders, warehouses)
-		self.assertEqual(inventoryAllocator.allocateInventory(), expected)
+    def test_multiple_order(self):
+        order = {'apple': 10, 'orange': 5}
+        warehouses = [{'name': 'wh1', 'inventory': {'apple': 10, 'orange': 5}}]
+        expected = [{'wh1': {'apple': 10, 'orange': 5}}]
+        inventoryAllocator = InventoryAllocator(warehouses)
+        self.assertEqual(inventoryAllocator.allocateInventory(order), expected)
 
-	def test_multiple_order_with_zero_order(self):
-		orders = {'apple': 10 , 'orange': 0}
-		warehouses = [{'name': 'wh1', 'inventory': {'apple': 10, 'orange': 5}}]
-		expected = [{'wh1': {'apple': 10 }}]
-		inventoryAllocator = InventoryAllocator(orders, warehouses)
-		self.assertEqual(inventoryAllocator.allocateInventory(), expected)
+    def test_multiple_order_with_zero_order(self):
+        order = {'apple': 10, 'orange': 0}
+        warehouses = [{'name': 'wh1', 'inventory': {'apple': 10, 'orange': 5}}]
+        expected = [{'wh1': {'apple': 10}}]
+        inventoryAllocator = InventoryAllocator(warehouses)
+        self.assertEqual(inventoryAllocator.allocateInventory(order), expected)
 
-	def test_multiple_order_items_multiple_warehouses(self):
-		orders = {'apple': 10 , 'orange': 5}
-		warehouses = [{'name': 'wh1', 'inventory': {'apple': 10}}, 
-					  {'name': 'wh2', 'inventory': {'orange': 5}}]
-		expected = [{'wh1': {'apple': 10}}, {'wh2': {'orange': 5}}]
-		inventoryAllocator = InventoryAllocator(orders, warehouses)
-		self.assertEqual(inventoryAllocator.allocateInventory(), expected)		
+    def test_multiple_order_items_multiple_warehouses(self):
+        order = {'apple': 10, 'orange': 5}
+        warehouses = [{'name': 'wh1', 'inventory': {'apple': 10}},
+                      {'name': 'wh2', 'inventory': {'orange': 5}}]
+        expected = [{'wh1': {'apple': 10}}, {'wh2': {'orange': 5}}]
+        inventoryAllocator = InventoryAllocator(warehouses)
+        # inventoryAllocator.allocateInventory(order)
+        self.assertEqual(inventoryAllocator.allocateInventory(order), expected)
 
-	def test_multiple_order_skip_warehouses(self):
-		orders = {'apple': 10 , 'orange': 5}
-		warehouses = [{'name': 'wh1', 'inventory': {'banana': 10}}, 
-					  {'name': 'wh2', 'inventory': {'apple': 10 , 'orange': 5}}]
-		expected = [{'wh2': {'apple': 10 , 'orange': 5}}]
-		inventoryAllocator = InventoryAllocator(orders, warehouses)
-		self.assertEqual(inventoryAllocator.allocateInventory(), expected)	
+    def test_reverse_multiple_order_items_multiple_warehouses(self):
+        order = {'orange': 5, 'apple': 10}
+        warehouses = [{'name': 'wh1', 'inventory': {'apple': 10}},
+                      {'name': 'wh2', 'inventory': {'orange': 5}}]
+        expected = [{'wh1': {'apple': 10}}, {'wh2': {'orange': 5}}]
+        inventoryAllocator = InventoryAllocator(warehouses)
+        self.assertEqual(inventoryAllocator.allocateInventory(order), expected)
 
+    def test_multiple_order_multiple_warehouses_by_cost(self):
+        order = {'apple': 10, 'orange': 7}
+        warehouses = [{'name': 'wh1', 'inventory': {'apple': 3, 'orange': 4}},
+                      {'name': 'wh2', 'inventory': {'apple': 1, 'orange': 0}},
+                      {'name': 'wh3', 'inventory': {'apple': 7, 'orange': 6}}]
+        expected = [{'wh1': {'apple': 3, 'orange': 4}},
+                    {'wh2': {'apple': 1}},
+                    {'wh3': {'apple': 6, 'orange': 3}}]
+        inventoryAllocator = InventoryAllocator(warehouses)
+        self.assertEqual(inventoryAllocator.allocateInventory(order), expected)
 
+    def test_multiple_order_skip_warehouses(self):
+        order = {'apple': 10, 'orange': 5}
+        warehouses = [{'name': 'wh1', 'inventory': {'banana': 10}},
+                      {'name': 'wh2', 'inventory': {'apple': 10, 'orange': 5}}]
+        expected = [{'wh2': {'apple': 10, 'orange': 5}}]
+        inventoryAllocator = InventoryAllocator(warehouses)
+        self.assertEqual(inventoryAllocator.allocateInventory(order), expected)
 
 
 if __name__ == '__main__':
-	unittest.main()
-
-
-
-	# def test_(self):
-	# 	orders = {}
-	# 	warehouses = []
-	# 	expected = []
-	# 	inventoryAllocator = InventoryAllocator(orders, warehouses)
-	# 	self.assertEqual(inventoryAllocator.allocateInventory(), expected)	
+    unittest.main()
